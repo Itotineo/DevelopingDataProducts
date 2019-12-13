77f8dbc394f61701822ee49328dea6aba281dd7a
Alexandros <tzalex@yahoo.com>
2019-12-14 01:07:13 +0200

    server.R and ui.R for the Shiny application

---
+++ Course_Project/server.R
@@ -0,0 +1,27 @@
+library(shiny)
+library(dplyr)
+library(readr)
+library(ggplot2)
+
+shinyServer(function(input, output) {
+    
+    IMDB_Movie_Data <- read_csv("IMDB-Movie-Data.csv") # dataset from https://www.kaggle.com/PromptCloudHQ/imdb-data 
+    data_movies <- IMDB_Movie_Data[,c(2,7,8,9,10,11)] # select some of the parameters
+    names(data_movies)[3] <- "Runtime_Minutes" # rename runtime column
+    names(data_movies)[6] <- "Revenue_MillionUSD" # rename revenue column
+    
+    # the output graph
+    output$plot1 <- renderPlot({
+        x_axis <- input$radiobuttons_x # input from radio buttons x
+        y_axis <- input$radiobuttons_y # input from radio buttons y
+        color_axis <- input$radiobuttons_color # input from radio buttons color
+        
+        # plot with ggplot
+        gg <- ggplot(data = data_movies,
+                     aes_string(x=x_axis, y=y_axis, color=color_axis))
+        gg <- gg + geom_point() + scale_color_gradientn(colors = rainbow(4))
+        gg
+        })
+})
+
+
---
+++ Course_Project/ui.R
@@ -0,0 +1,36 @@
+shinyUI(fluidPage(
+    
+    titlePanel("Developing Data Products - Course Project: Shiny Application and Reproducible Pitch"),
+
+    sidebarLayout(
+        sidebarPanel(
+            
+            # radio buttons x
+            radioButtons(inputId = "radiobuttons_x",
+                         label = h3("Select x-axis"),
+                         choices = c("Year","Runtime_Minutes", "Rating", "Votes", "Revenue_MillionUSD"),
+                         selected = "Rating"),
+            
+            # radio buttons y
+            radioButtons(inputId = "radiobuttons_y",
+                         label = h3("Select y-axis"),
+                         choices = c("Year","Runtime_Minutes", "Rating", "Votes", "Revenue_MillionUSD"),
+                         selected = "Votes"),
+            
+            # radio buttons color
+            radioButtons(inputId = "radiobuttons_color",
+                         label = h3("Select color parameter"),
+                         choices = c("Year","Runtime_Minutes", "Rating", "Votes", "Revenue_MillionUSD"),
+                         selected = "Year"),
+        ),
+        
+        mainPanel(
+            h4("This is the first part of the assignement (the Shiny application)."),
+            h5("It's a graph of a movies dataset. You are able to control the graph by selecting the x-axis, the y-axis and the color parameter. The graph changes everytime a new parameter is selected."),
+            h5("The dataset contains data of 1000 popular movies on IMDB from 2006 to 2016. I have chosen some parameters from the dataset: The year that the film released, the duration of the film in minutes, user rating for the movie 0-10, number of votes, and movie revenue in million US dollars."),
+            
+            # THE GRAPH
+            plotOutput("plot1"))
+    )
+    ))
+
